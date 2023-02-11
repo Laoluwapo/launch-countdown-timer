ca328aacd4482e1ef2ea03a93bb9912e506cc79c
laoluwapo <alividegabriel9@gmail.com>
2023-02-11 14:54:13 +0100

    Add js code that checks when hours, minutes & seconds are < 10 then puts 0 before it.

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ const countdown = () => {
 
   // Calculate the actual time
   const textDay = Math.floor(gap / day);
-  const textHours = Math.floor((gap % day) / hour);
-  const textMinutes = Math.floor((gap % hour) / minute);
-  const textSeconds = Math.floor((gap % minute) / second);
+  let textHours = Math.floor((gap % day) / hour);
+  let textMinutes = Math.floor((gap % hour) / minute);
+  let textSeconds = Math.floor((gap % minute) / second);
+
+  // Check of hours, minutes & seconds are < 10
+  if (textHours < 10) textHours = `0${textHours}`;
+  if (textMinutes < 10) textMinutes = `0${textMinutes}`;
+  if (textSeconds < 10) textSeconds = `0${textSeconds}`;
 
   // Get the time elements from html and update it
   document.querySelector(".days").textContent = textDay;
